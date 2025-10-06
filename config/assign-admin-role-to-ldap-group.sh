75f548275f84b8bdde929056d568319ba41b6445
Atlanta Local projects <dmitrykus@gmail.com>
2025-10-06 10:05:10 -0400

    update admin role script

--- config/assign-admin-role-to-ldap-group.sh
+++ config/assign-admin-role-to-ldap-group.sh
@@ -25,15 +25,18 @@ if [[ -z "$GROUP_ID" ]]; then
 fi
 
 echo " Found group '$LDAP_GROUP_NAME' with ID: $GROUP_ID"
+# Run manual for testing
+/opt/keycloak/bin/kcadm.sh get clients -r $REALM --fields id,clientId | grep realm-management
 
 # Get the "admin" realm role (from master realm’s "realm-management" client)
 CLIENT_ID=$(/opt/keycloak/bin/kcadm.sh get clients -r $REALM \
   -q clientId=realm-management --fields id --format csv | tail -n +2)
+# Ged data with JSON format
+CLIENT_ID=$(/opt/keycloak/bin/kcadm.sh get clients -r $REALM --query clientId=realm-management --fields id | sed -n 's/.*"id" : "\(.*\)".*/\1/p')
 
 ROLE_JSON=$(/opt/keycloak/bin/kcadm.sh get clients/$CLIENT_ID/roles/admin -r $REALM)
 
 # Assign the "admin" role to the LDAP group
 echo "$ROLE_JSON" | /opt/keycloak/bin/kcadm.sh create groups/$GROUP_ID/role-mappings/clients/$CLIENT_ID \
-  -r $REALM -f -
-
+  -r $REALM -f -+
 echo "Group '$LDAP_GROUP_NAME' assigned Keycloak admin role successfully."
